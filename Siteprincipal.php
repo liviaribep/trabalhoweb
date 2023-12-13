b80f63007a0dc73dfedb9eae477cf5ca6e8c1f45
Lívia Ribeiro Pessanha <liviaribep@gmail.com>
2023-12-13 14:41:27 -0300

    atualização do site principal de administrador

--- Siteprincipal.php
+++ Siteprincipal.php
@@ -11,58 +11,44 @@
     padding: 0;
 }
 
-header {
-    background-color:  rgb(200, 200, 200);
-    color: rgb(200, 200, 200);
-    position: absolute;
-    left: 0px; top: 0px;
-    width: 100%;
-}
+ul.menu {
+            list-style-type: none;
+            padding: 0;
+            margin: 0;
+            background-color: #333;
+            overflow: hidden;
+        }
 
-header h1 {
-    margin: 0;
-    display: inline;
-}
+        ul.menu li {
+            float: left;
+        }
 
+        ul.menu li a {
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
 
-ul {
-    list-style: none;
-    margin: 0;
-    padding: 0;
-}
+        ul.menu li a:hover {
+            background-color: #111;
+        }
 
-ul li {
-    display: inline;
-    margin-right: 20px;
-}
-
-ul li a {
-    text-decoration: none;
-    color: #fff;
-    font-weight: bold;
-    font-size: 18px;
-}
-</style>
+        h1 {
+            position: relative;
+            top: 50px;
+            left: 23%;
+        }
+    </style>
 </head>
 <body>
-
-    <header>
-            <ul>
-                <li><a href="index.html">Home</a></li>
-                <li><a href="loja.html">Loja</a></li>
-                <li><a href="adicionar.html">Adicionar Produtos</a></li>
-                <li><a href="admins.html">Administradores</a></li>
-                <li><a href="sobre.html">Sobre</a></li>
-            </ul>
-    </header>
-
-    <main>
-        <h1>Bem-vindo à Página Inicial!</h1>
-
-
-       
-        
-    </main>
-
+    <ul class="menu">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#news">Notícias</a></li>
+        <li><a href="#contact">Contato</a></li>
+        <li><a href="#about">Sobre</a></li>
+    </ul>
+    <h1> BEM VINDO A PAGINA DE ADMINISTRADORES </h1>
 </body>
 </html>
