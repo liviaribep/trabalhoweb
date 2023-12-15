1cdd8f6131f2f5689b1f200357ad4c454636353a
Lívia Ribeiro Pessanha <148587005+liviaribep@users.noreply.github.com>
2023-12-15 14:51:31 -0300

    Update Clientes.php

--- Clientes.php
+++ Clientes.php
@@ -3,9 +3,62 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Lista de Clientes</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+        }
+
+        header {
+            background-color: #333;
+            padding: 10px;
+            color: white;
+            text-align: center;
+        }
+
+        section {
+            padding: 20px;
+        }
+
+        ul {
+            list-style-type: none;
+            padding: 0;
+        }
+
+        li {
+            background-color: white;
+            border: 1px solid #ddd;
+            padding: 20px;
+            margin-bottom: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+    </style>
 </head>
 <body>
-    
+
+    <header>
+        <h1>Lista de Clientes</h1>
+    </header>
+
+    <section>
+        <ul>
+            <li>
+                <h2>Cliente 1</h2>
+                <p>Nome: João Silva</p>
+                <p>Email: joao@example.com</p>
+                <p>Telefone: (11) 1234-5678</p>
+            </li>
+
+            <li>
+                <h2>Cliente 2</h2>
+                <p>Nome: Maria Oliveira</p>
+                <p>Email: maria@example.com</p>
+                <p>Telefone: (11) 9876-5432</p>
+            </li>
+        </ul>
+    </section>
 </body>
 </html>
